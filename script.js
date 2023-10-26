740a20222262f38b5dbc64ceafa8d28fd854e96f
TylerD1084 <mario.markwart@gmail.com>
2023-10-26 14:06:41 +0200

    JS implementiert

--- script.js
+++ script.js
@@ -16,3 +16,41 @@ function sendMail(event) {
       console.log(error);
     });
 }
+
+let originAmounts = [];
+function getInputAmount() {
+
+  //get the value of inputfield 
+  let inputAmount = document.getElementById('ing-input').value
+  console.log(`Zutaten für: ${inputAmount}`);
+
+  // find all classes with 'ing-amount'
+  let amounts = document.getElementsByClassName('ing-amount');
+
+  //saving the original amount-values only on the first run
+  if (originAmounts.length == 0) {
+    for (let i = 0; i < amounts.length; i++) {
+      originAmounts.push(amounts[i].innerHTML);
+    }
+  }
+  //iterate through the array
+  for (let i = 0; i < amounts.length; i++) {
+    console.log('origin: ' + originAmounts[i]);
+
+    //reset the actual to the original value to calculate with it
+    amounts[i].innerHTML = originAmounts[i];
+
+    //the resetet value can be multiplied with the inputAmount
+    //if amount gets zero
+    if (amounts[i].innerHTML == 0) {
+
+      amounts[i].innerHTML = '';
+    }
+    else {
+      amounts[i].innerHTML = amounts[i].innerHTML / 4 * inputAmount;
+      console.log('amounts: ' + amounts[i].innerHTML);
+    }
+  }
+}
+
+
